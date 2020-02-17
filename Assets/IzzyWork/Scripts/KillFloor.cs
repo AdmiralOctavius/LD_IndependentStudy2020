e8823d9c53c4d327bbdd9b3d2c05062bfeac8963
AdmiralOct <buildgamessnotbombs@gmail.com>
2020-02-17 04:55:21 -0500

    First pass on Week 5 done
    Week 6 started as well

---
+++ Assets/IzzyWork/Scripts/KillFloor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillFloor : MonoBehaviour
+{
+
+    public GameObject spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.position = spawnPoint.transform.position;
+        }
+    }
+}
