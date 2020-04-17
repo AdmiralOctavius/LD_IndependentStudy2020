b5e1440cfa68cb636fa3369014a2c3835eca5b51
AdmiralOct <buildgamessnotbombs@gmail.com>
2020-04-17 18:41:38 -0400

    Crate and Observer

---
+++ Assets/Scripts/ScannerObj.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScannerObj : MonoBehaviour
+{
+
+    public GameObject startPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Got here, Obj is: " + col.gameObject.name);
+
+
+        RaycastHit newRay;
+
+        //Docs to source of direction code
+        //https://docs.unity3d.com/Manual/DirectionDistanceFromOneObjectToAnother.html
+        var heading = col.gameObject.transform.position - startPoint.gameObject.transform.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance;
+        if(Physics.Raycast(startPoint.gameObject.transform.position, direction, out newRay))
+        {
+            Debug.Log("Got raycast");
+            Debug.DrawRay(startPoint.gameObject.transform.position, direction, Color.blue, 10,false);
+            Debug.Log("Hit obj was: " + newRay.collider.name);
+            if(newRay.collider.tag == "Player")
+            {
+                this.gameObject.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
---
+++ Assets/cratePickup.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cratePickup : MonoBehaviour
+{
+
+    public GameObject camera;
+    public GameObject crateHolder;
+    public GameObject crate;
+    public bool crateHeld;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (crateHeld)
+        {
+            //create check here to have object he held higher above head if camera angle is -20x
+            crate.transform.position = crateHolder.transform.position;
+
+        }
+
+        if (Input.GetButtonDown("Interact"))
+        {
+            if (!crateHeld)
+            {
+                RaycastHit newRay;
+                Debug.Log("Button pressed");
+
+                if (Physics.Raycast(camera.transform.position, camera.transform.forward, out newRay, 20f))
+                {
+                    Debug.Log("Got raycast");
+                    Debug.DrawRay(camera.gameObject.transform.position, 100 * camera.transform.forward, Color.red, 10, false);
+                    Debug.Log("Hit obj was: " + newRay.collider.name);
+
+                    if (newRay.collider.gameObject.tag == "Crate")
+                    {
+                        crate = newRay.collider.gameObject;
+                        crate.GetComponent<Rigidbody>().useGravity = false;
+                        crateHeld = true;
+                        crate.GetComponent<Rigidbody>().freezeRotation = true;
+                    }
+                }
+            }
+            else
+            {
+                crateHeld = false;
+                crate.GetComponent<Rigidbody>().useGravity = true;
+                crate.GetComponent<Rigidbody>().freezeRotation = false;
+                crate = null;
+            }
+        }
+    }
+
+
+}
