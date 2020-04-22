681e300e45afb9dda4df775b67c74786e380c0bc
AdmiralOct <buildgamessnotbombs@gmail.com>
2020-04-22 02:45:07 -0400

    Continued work on chap8
    need to finish rotator script

---
+++ Assets/EnemyRotator.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRotator : MonoBehaviour
+{
+    public float rot1;
+    public float rot2;
+
+    public bool rotated = false;
+
+    public float speed = 1.0f;
+
+    private float startTime;
+    private float journeyLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        startTime = Time.time;
+        journeyLength = this.transform.rotation.y - rot1;
+        Debug.Log(journeyLength);
+        //StartCoroutine("rotateTo1");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //READ THIS IN MORNING
+        //https://answers.unity.com/questions/1204245/rotating-smoothly-between-two-angles-mathematical.html
+        Vector3 to = new Vector3(0, rot1, 0);
+        if (this.gameObject.transform.rotation.y <= rot1)
+        {
+
+        }
+        if(this.gameObject.transform.rotation.y >= rot1)
+        {
+            transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, 0.1f);
+        }
+        else if (this.gameObject.transform.rotation.y != rot1)
+        {
+
+            float distCovered = (Time.time - startTime) * speed;
+            float fractionOfJourney = distCovered / journeyLength;
+            transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, fractionOfJourney);
+        }
+    }
+
+    IEnumerator rotateTo1()
+    {
+        int i = 10;
+        Vector3 to = new Vector3(0, rot1, 0);
+
+        
+
+        while(this.gameObject.transform.rotation.y <= rot1)
+        {
+            i++;
+            if(i >= 10)
+            {
+                yield return new WaitForSeconds(.1f);
+            }
+            float distCovered = (Time.time - startTime) * speed;
+            float fractionOfJourney = distCovered / journeyLength;
+            transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, fractionOfJourney);
+        }
+        //Should end it
+        yield return null;
+    }
+}
--- Assets/Scripts/ScannerObj.cs
+++ Assets/Scripts/ScannerObj.cs
@@ -20,7 +20,7 @@ public class ScannerObj : MonoBehaviour
 
     void OnTriggerEnter(Collider col)
     {
-        Debug.Log("Got here, Obj is: " + col.gameObject.name);
+       // Debug.Log("Got here, Obj is: " + col.gameObject.name);
 
 
         RaycastHit newRay;
@@ -32,12 +32,37 @@ public class ScannerObj : MonoBehaviour
         var direction = heading / distance;
         if(Physics.Raycast(startPoint.gameObject.transform.position, direction, out newRay))
         {
-            Debug.Log("Got raycast");
-            Debug.DrawRay(startPoint.gameObject.transform.position, direction, Color.blue, 10,false);
-            Debug.Log("Hit obj was: " + newRay.collider.name);
+           // Debug.Log("Got raycast");
+          //  Debug.DrawRay(startPoint.gameObject.transform.position, direction, Color.blue, 10,false);
+          //  Debug.Log("Hit obj was: " + newRay.collider.name);
             if(newRay.collider.tag == "Player")
             {
                 this.gameObject.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        //Debug.Log("Got here, Obj is: " + col.gameObject.name);
+
+
+        RaycastHit newRay;
+
+        //Docs to source of direction code
+        //https://docs.unity3d.com/Manual/DirectionDistanceFromOneObjectToAnother.html
+        var heading = col.gameObject.transform.position - startPoint.gameObject.transform.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance;
+        if (Physics.Raycast(startPoint.gameObject.transform.position, direction, out newRay))
+        {
+            //Debug.Log("Got raycast");
+           // Debug.DrawRay(startPoint.gameObject.transform.position, direction, Color.blue, 10, false);
+            //Debug.Log("Hit obj was: " + newRay.collider.name);
+            if (newRay.collider.tag == "Player")
+            {
+                if(this.GetComponent<AudioSource>().isPlaying ==false)
+                    this.gameObject.GetComponent<AudioSource>().Play();
             }
         }
     }
