80bc8ffdbc369f0d0d9690d1226b45ec92cee29d
AdmiralOct <buildgamessnotbombs@gmail.com>
2020-04-29 23:17:42 -0400

    Finished IntimateSpace
    Created main menu
    Added Credits
    Need to add credits for FPS controller
    Add footstep sounds and improve speed

---
+++ Assets/ButtonPress.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonPress : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButton()
+    {
+
+        SceneManager.LoadScene("BranchingFlytrap");
+
+    }
+
+
+    public void ExitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
